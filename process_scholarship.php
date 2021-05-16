c2bd900c3091ca14054f54a32a19ee6003f4f659
hoang000147 <l_hoang07@yahoo.com>
2021-05-16 10:00:24 +0700

    first commit

---
+++ process_scholarship.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Scholarships</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+</head>
+<body>
+<?php
+    
+    //$firstName = $_GET['fName'];
+    //$lastName = $_GET['lName'];
+    //$firstName = $_POST['fName'];
+    //$lastName = $_POST['lName'];
+    $firstName = $_REQUEST['fName'];
+    $lastName = $_REQUEST['lName'];
+    echo "Thank you for filling out the scholarship form, " . $firstName . " " . $lastName . ".";
+?>
+   
+</body>
+</html>
+
