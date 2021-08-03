9c66777f3fc7ad9d042cb2705d8d728b0ea01cb7
hoang000147 <l_hoang07@yahoo.com>
2021-08-03 20:32:32 +0700

    Create keys.js

---
+++ server/config/keys.js
@@ -0,0 +1,22 @@
+const SERVER = {
+    secret:"n69zhKGLv6",
+    port: 3000
+}
+
+const DB_USER = "test";
+const DB_PASSWORD = "password";
+const MONGODB = {
+    MONGODB_URI: `mongodb://${DB_USER}:${DB_PASSWORD}@ds<SOME_DOMAIN>.mlab.com:<PORT>/<PROJECT_NAME>`
+};
+
+const SESSION = {
+    COOKIE_KEY: "romeo"
+};
+
+const KEYS = {
+    ...MONGODB,
+    ...SESSION,
+    ...SERVER
+}
+
+module.exports = KEYS;
